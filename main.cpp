f0f272fa7d1a73111df1d1b2c44fca8a84c583d2
Carl Gombert <carlgombert2@gmail.com>
2025-03-04 16:14:33 -0600

    OpenGL boilerplate

--- main.cpp
+++ main.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
+
+int WIDTH = 800, HEIGHT = 600;
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    glfwInit();
+
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "OpenGL Window", nullptr, nullptr);
+    if (window == nullptr) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glViewport(0, 0, WIDTH, HEIGHT);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0, WIDTH, HEIGHT, 0, -1, 1);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    // Implement some sort of FPS/update system? right now I think it will render as many frames as fast as possible
+    // set this loop to run like 24 times per second
+    // maybe implement separate update system for the reaction, that way it can be slowed down/sped up
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Draw in here
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
